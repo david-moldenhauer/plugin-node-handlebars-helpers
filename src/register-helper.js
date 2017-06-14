4036b0eeb5c74f3443aa25303149478f7da7acc9
David Moldenhauer <david.moldenhauer@interone.de>
2017-06-14 12:50:31 +0200

    imgInterchange functioning

--- src/register-helper.js
+++ src/register-helper.js
@@ -1,22 +1,18 @@
 'use strict';
 
 function registerHelper(patternlab, Handlebars) {
-  Handlebars.registerHelper('responsiveImage', function(options) {
-    let html = '';
-    let src = patternlab.responsiveImage[options.hash.src];
-    let suffix = patternlab.responsiveImage[options.hash.suffix];
-    html = options.fn(src);
-    html += ', suffix: ';
-    html += options.fn(suffix);
-    /*<img data-interchange="[{{imageName}}-520.jpg, small], [{{imageName}}-980.jpg, medium], [{{imageName}}-1680.jpg, large]">
-    responsiveImage.forEach(function(listitem) {
-      if (patternlab.config.debug) {
-        console.log(listitem);
-      }
-      html = html + options.fn(listitem)
-    })*/
-    return html;
-  });
+    Handlebars.registerHelper('imgInterchange', function(options) {
+        let html = '';
+        let src = options.hash.src;
+        let breakpoints = options.hash.breakpoint.split(/[\,\;\| ]+/);
+        html += '<img data-interchange="';
+        breakpoints.forEach(function(breakpoint) {
+            html += src.replace(/([^\s]+)(\.)(jpg|png|gif|bmp)$/, '[$1-'+breakpoint+'.$3, '+breakpoint+'],');
+        });
+        html = html.substring(0,html.length-1);
+        html += '">'
+        return new Handlebars.SafeString(html);
+    });
 }
 
 module.exports = registerHelper;
